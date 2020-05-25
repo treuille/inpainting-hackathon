e2cc6449b1a1788d50c7a9ce6ebccf9ff15c14f2
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-05-25 09:41:42 -0700

    Got the mask drawn properly

--- inpainting.py
+++ inpainting.py
@@ -52,18 +52,23 @@ st.image(image)
 'mininum', np.amin(image.flat)
 'maximum', np.amax(image.flat)
 
+mask = np.zeros(image.shape[:2], dtype=np.int32)
+mask[image[:,:,0] > 0] = 255
+'mask:', mask.dtype
+st.image(mask)
+
 raise RuntimeError('Early stopping.')
 
-# It can live in the sidebar.
-num_clicks = st.sidebar.my_component("Sidebar")
-st.sidebar.markdown("You've clicked %s times!" % int(num_clicks))
-
-name_input = st.text_input("Enter a name", value="Streamlit")
-
-# Use the special "key" argument to assign your component a fixed identity
-# if you want to change its arguments over time and not have it be
-# re-created. (If you remove the "key" argument here, then the component will
-# be re-created whenever a new name is entered in 'name_input', which means
-# it will lose its current state.)
-num_clicks = st.my_component(name_input, key="foo")
-st.markdown("You've clicked %s times!" % int(num_clicks))
+# # It can live in the sidebar.
+# num_clicks = st.sidebar.my_component("Sidebar")
+# st.sidebar.markdown("You've clicked %s times!" % int(num_clicks))
+# 
+# name_input = st.text_input("Enter a name", value="Streamlit")
+# 
+# # Use the special "key" argument to assign your component a fixed identity
+# # if you want to change its arguments over time and not have it be
+# # re-created. (If you remove the "key" argument here, then the component will
+# # be re-created whenever a new name is entered in 'name_input', which means
+# # it will lose its current state.)
+# num_clicks = st.my_component(name_input, key="foo")
+# st.markdown("You've clicked %s times!" % int(num_clicks))
